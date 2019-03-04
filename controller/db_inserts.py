3a4282bef5bb9528578a3b8d8aa48234357214a4
David Bermejo <dbermejosimon@gmail.com>
2019-03-04 14:46:38 +0100

    implementado registro de movimientos a producción

--- controller/db_inserts.py
+++ controller/db_inserts.py
@@ -7,22 +7,101 @@ INSERCIÓN DE MOVIMIENTOS
 """
 import mysql.connector
 
-from ProductoAlmacen import ProductoAlmacen
+from model.ProductoAlmacen import ProductoAlmacen
 
 
 class DBInsertMovement:
 
     def insertar_movimiento(self):
-        print("Inserta el codigo de producto que desee producir: ")
-        materials = []
+        print("Inserta el codigo de producto que desee producir (Debe existir en la tabla de despiece): ")
         codigo = input()
-        res = self.getColumnAndCode("despiece", "C2")
+        print("Inserta la cntidad que quieres producir:")
+        cantidad_solicitada = int(input())
+        make_movement = True
+
+        materials = []
+        res = self.check_codigo(self, codigo)
         if res is not None:
             for row in res:
-                materials += [row[1], row[2]]
-            print(materials)
+                if self.check_materials(self, row[1], row[2] * cantidad_solicitada) is True:
+                    product = ProductoAlmacen(row[1], row[2] * cantidad_solicitada, None)
+                    materials.append(product)
+                else:
+                    print("\n[Lo siento, no hay suficiente cantidad del material con codigo " + row[1] + "]")
+                    make_movement = False
+
+            if make_movement is True:
+                self.discount_materials(self, materials)
+                self.produce_product(self, codigo, cantidad_solicitada)
+                self.insert_log_movement(self, codigo, cantidad_solicitada)
+            else:
+                print("\n---Cancelando operación de movimiento a producción---")
         else:
+            print("\n[Lo siento, ese codigo no se encuentra en la tabla de despiece]")
             print("---Cancelando operación de movimiento a producción---")
+
+    def check_materials(self, codigo, cantidad):
+        db = mysql.connector.connect(
+            host="localhost",
+            database="db_sge",
+            user="root",
+            password=""
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT cantidad FROM almacen1 WHERE codigo='" + codigo + "'")
+        res = cursor.fetchone()
+        cantidad_exist = int(res[0])
+        if cantidad_exist >= cantidad:
+            return True
+        else:
+            return False
+        db.close()
+
+    def discount_materials(self, materials):
+        db = mysql.connector.connect(
+            host="localhost",
+            database="db_sge",
+            user="root",
+            password=""
+        )
+        print("\n DESCONTANDO UNIDADES DEL ALMACEN DE MATERIALES (ALMACEN 1)")
+        for product in materials:
+            cursor = db.cursor()
+            cursor.execute("UPDATE almacen1 SET cantidad = cantidad - %s WHERE codigo=%s ",
+                           (product.cantidad, product.codigo))
+            print("\t[Descontadas " + str(product.cantidad) + " unidades del material con codigo " + product.codigo +
+                  " del almacen1]")
+            db.commit()
+        db.close()
+
+    def produce_product(self, codigo, cantidad):
+        db = mysql.connector.connect(
+            host="localhost",
+            database="db_sge",
+            user="root",
+            password=""
+        )
+        cursor = db.cursor()
+        print("\n PRODUCIENDO UNIDADES EN EL ALMACEN DE PRODUCCION(ALMACEN 2)")
+        cursor.execute("UPDATE almacen2 SET cantidad = cantidad + %s WHERE codigo=%s ", (cantidad, codigo))
+        print("\t[Producidas " + str(cantidad) + " unidades del producto con codigo " + codigo)
+        db.commit()
+        db.close()
+
+    def insert_log_movement(self, codigo, cantidad):
+        db = mysql.connector.connect(
+            host="localhost",
+            database="db_sge",
+            user="root",
+            password=""
+        )
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO movimientos VALUES(null,SYSDATE(),'"+codigo+"',"+str(cantidad)+")")
+        print("\nREGISTRANDO MOVIMIENTO EN LA TABLA DE LOG")
+        print("\t[Registrado movimiento satisfactoriamente]")
+        db.commit()
+        db.close()
+
     def insertar_almacen1(self):
         db = mysql.connector.connect(
             host="localhost",
@@ -30,9 +109,14 @@ class DBInsertMovement:
             user="root",
             password=""
         )
-        res, codigo = self.getColumnAndCode("almacen1", "codigo")
+        print("---Introduce el codigo del material del que quieras introducir más unidades---")
+        codigo = input()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM almacen1 WHERE codigo='" + codigo + "'")
+        res = list(cursor)
         if res is not None:
-            cantidad = self.obtener_cantidad()
+            print("---¿Cuantas unidades desea ingresar?---")
+            cantidad = int(input())
             cursor = db.cursor()
             cursor.execute("""
                UPDATE almacen1
@@ -44,27 +128,21 @@ class DBInsertMovement:
             print("---Cantidad introducida correctamente---")
         else:
             print("---No se ha encontrado el codigo introducido, volviendo al menú principal---")
-    def getColumnAndCode(tabla, column):
+        db.close()
+
+    def check_codigo(self, codigo):
         db = mysql.connector.connect(
             host="localhost",
             database="db_sge",
             user="root",
             password=""
         )
-        print("---Introduce el codigo del material del que quieras introducir más unidades---")
-        codigo = input()
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM " + tabla + " WHERE " + column + "='" + codigo + "'")
-        res = list(cursor)
-        if len(res) == 0:
-            print("Lo siento, ese codigo no se encuentra en la tabla ",tabla)
-            return None, codigo
+        cursor.execute("SELECT * FROM despiece WHERE C2='" + codigo + "'")
+        res = cursor.fetchall()
+        res_list = list(res)
+        if len(res_list) == 0:
+            return None
         else:
-            product = ProductoAlmacen()
-            product.setValues(res[0])
-            print(product)
-            return res, codigo
-    def obtener_cantidad():
-        print("---¿Cuantas unidades desea ingresar?---")
-        cantidad = int(input())
-        return cantidad
+            db.close()
+            return res
